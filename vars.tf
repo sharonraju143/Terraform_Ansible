8f06f1bb8a0433674b343b45745cd069bc43d398
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2023-05-23 12:11:11 +0530

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,16 @@
+variable "location" {
+    type = string
+    default = "East US 2"
+  
+}
+
+variable "resource_group_name" {
+    type = string
+    default = "k8s_cluster"
+  
+}
+variable "environment" {
+    type = string
+    default = "Dev"
+
+}
